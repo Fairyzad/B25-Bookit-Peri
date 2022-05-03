a6677666337270aff0418ad520196b1c86e95ca9
Jamal <jamal@cybertekschool.com>
2022-05-03 11:20:14 -0400

    1st scenario

--- src/test/java/com/bookit/step_definitions/ApiStepDefs.java
+++ src/test/java/com/bookit/step_definitions/ApiStepDefs.java
@@ -1,17 +1,28 @@
 package com.bookit.step_definitions;
 
+import com.bookit.utilities.ConfigurationReader;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 
-import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
 
 public class ApiStepDefs {
 
     @Given("I logged Bookit api using {string} and {string}")
-    public void i_logged_Bookit_api_using_and(String string, String string2) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+    public void i_logged_Bookit_api_using_and(String email, String password) {
+
+        Response response = given()
+                .accept(ContentType.JSON)
+                .queryParam("email", email)
+                .and()
+                .queryParam("password", password)
+                .when().get(ConfigurationReader.get("base_url") + "/sign");
+
+        response.prettyPrint();
+
     }
 
     @When("I get the current user information from api")
