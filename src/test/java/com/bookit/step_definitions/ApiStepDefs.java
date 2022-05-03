7b6c26091e1d93bd89086d69968706857924e279
Jamal <jamal@cybertekschool.com>
2022-05-03 11:36:14 -0400

    1st scenario

--- src/test/java/com/bookit/step_definitions/ApiStepDefs.java
+++ src/test/java/com/bookit/step_definitions/ApiStepDefs.java
@@ -1,5 +1,6 @@
 package com.bookit.step_definitions;
 
+import com.bookit.utilities.BookitUtils;
 import com.bookit.utilities.ConfigurationReader;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -11,24 +12,25 @@ import static io.restassured.RestAssured.*;
 
 public class ApiStepDefs {
 
+    String token;
+
     @Given("I logged Bookit api using {string} and {string}")
     public void i_logged_Bookit_api_using_and(String email, String password) {
 
-        Response response = given()
-                .accept(ContentType.JSON)
-                .queryParam("email", email)
-                .and()
-                .queryParam("password", password)
-                .when().get(ConfigurationReader.get("base_url") + "/sign");
 
-        response.prettyPrint();
-
+         token = BookitUtils.generateToken(email,password);
     }
 
     @When("I get the current user information from api")
     public void i_get_the_current_user_information_from_api() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+
+        Response response = given().accept(ContentType.JSON)
+                .and()
+                .header("Authorization", token)
+                .when().get(ConfigurationReader.get("base_url") + "/api/users/me");
+
+        response.prettyPrint();
+
     }
 
     @Then("status code should be {int}")
